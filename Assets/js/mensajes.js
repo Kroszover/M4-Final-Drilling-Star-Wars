83a48ddd8cd6f01a54a4db961fa1e5f4b8e07513
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-05-22 20:31:28 -0400

    Delete mensajes.js

--- Assets/js/mensajes.js
+++
@@ -1,7 +0,0 @@
-export const Mensaje = {
-  estaSeccion: "En esta sección...",
-  findInfo: "Encontrarás información sobre ",
-  pjPrincipal: "los personajes más populares de las películas.",
-  pjSecundario: "personajes secundarios importantes.",
-  pjBlue: "Encontrarás otros personajes significativos.",
-};
